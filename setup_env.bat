5294e53b2fbe06bcfe203ec6f589776cdb342b6c
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2024-08-10 18:03:24 +0300

    fix: virtual enviroment issue

--- setup_env.bat
+++ setup_env.bat
@@ -1,16 +1,23 @@
 @echo off
 
+REM Check if Python is installed
+python --version >nul 2>&1
+if errorlevel 1 (
+    echo Python is not installed. Please install Python and try again.
+    pause
+    exit /b
+)
+
 REM Check if pip is available; if not, install it
 if not exist .\pythonembed\Scripts\pip.exe (
     echo Installing pip...
     .\pythonembed\python.exe get-pip.py
 )
 
-REM Check if virtual environment exists
+REM Check if virtual environment exists; if not, create it
 if not exist venv (
     echo Creating virtual environment...
-    mkdir venv
-    xcopy /E /I /Y myenv venv
+    python -m venv venv
 )
 
 REM Activate the virtual environment
